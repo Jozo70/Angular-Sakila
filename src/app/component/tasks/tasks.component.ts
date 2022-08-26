8a98a181436d4746bf451485afe27e3a6fe29611
Jozo70 <j.hollick4863@student.leedsbeckett.ac.uk>
2022-08-26 06:55:14 +0100

    Adjustments made following Tutorial

--- src/app/component/tasks/tasks.component.ts
+++ src/app/component/tasks/tasks.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 //import {TaskService} from 'src/app/services/task.service';
 
 import {Task} from 'src/app/Task';
-import {TASKS} from 'src/app/mock-tasks';
 
 @Component({
   selector: 'app-tasks',
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Task} from '../Task';
+import {TASKS} from '../mock-tasks';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+}
